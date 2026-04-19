6319f86509154c7c6dd6c96ccd470d6340ecda91
Z10N-exe <nwebedudon@gmail.com>
2026-04-19 01:52:55 +0100

    lambo truck outside

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,1 +1,21 @@
 const API_URL = 'https://moviezone-api.onrender.com';
+import { createContext, useContext, useState } from "react";
+
+const AppContext = createContext();
+
+// ✅ Provider
+export const AppProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [favorites, setFavorites] = useState([]);
+
+    return (
+        <AppContext.Provider value={{ user, setUser, favorites, setFavorites }}>
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+// ✅ THIS is what you are missing (causing the error)
+export const useAppContext = () => {
+    return useContext(AppContext);
+};
